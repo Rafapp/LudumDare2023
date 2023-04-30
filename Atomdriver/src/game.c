4ca2cf019f3a164f9898af3d5822bf16dbd444b8
Rafapp <rpadiper@gmail.com>
2023-04-29 17:26:21 -0700

    movement works, rotation works

--- Atomdriver/src/game.c
+++ Atomdriver/src/game.c
@@ -1,76 +1,48 @@
+// Raylib headers
 #include "raylib.h"
 #include "raymath.h"
 #include "rlgl.h"
 #include "stdio.h"
 
-// Window specs
-#define SCREEN_WIDTH (800)
-#define SCREEN_HEIGHT (450)
-#define WINDOW_TITLE "Atomdriver"
+// Script headers
+#include "rendering.h"
+#include "game.h"
+
+Vector3 truckPosition;
 
 int main(void)
 {
-    // Scale
-    float scale = 15;
-
-    // Camera settings
-    Camera3D camera = { 0 };
-    camera.position = (Vector3){ 20.0f, 20.0f, 20.0f };
-    camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };
-    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
-    camera.fovy = 45.0f;
-    camera.projection = CAMERA_ORTHOGRAPHIC;
-    UpdateCamera(&camera, CAMERA_FREE);
-
-    // Start window
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE);
-    SetTargetFPS(60);
-
-    // Load cube and its texture
-    Model cube = LoadModel("../assets/Models/TestModel.obj");
-    Texture2D texture = LoadTexture("../assets/Textures/TestTexture.png");
-    cube.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = texture;
-
-    Vector3 cubePosition = (Vector3){0,0,0};
-
-    // Skybox
-    Color topColor = {10, 63, 68, 255};
-    Color bottomColor = {67, 153, 114};
+    truckPosition = (Vector3){0,0,0};
+    RenderInit();
 
     // Movement
-    Vector3 moveHorizontal = (Vector3){scale/2,0,0};
-    Vector3 moveVertical = (Vector3){0,0,scale/2};
+    Vector3 moveHorizontal = (Vector3){scaleUnit/2,0,0};
+    Vector3 moveVertical = (Vector3){0,0,scaleUnit/2};
+    Vector3 rotationAxis = (Vector3){0, 1, 0};
 
     while (!WindowShouldClose())
     {
-        // Standard draw loop
-        BeginDrawing();
-            ClearBackground(topColor);
-            BeginMode3D(camera);
-                DrawModel(cube, cubePosition, scale, WHITE);
-            EndMode3D();
-        EndDrawing();
-
+        RenderLoop();
+        
         // Input
         if(IsKeyPressed(KEY_W) || IsKeyPressed(KEY_UP)){
-            cubePosition = Vector3Add(cubePosition, Vector3Negate(moveVertical));
+            truckPosition = Vector3Add(truckPosition, Vector3Negate(moveVertical));
+            truckModel.transform = MatrixRotateXYZ((Vector3){ 0.0f, DEG2RAD * 0, 0.0f });
         }
         if(IsKeyPressed(KEY_A) || IsKeyPressed(KEY_LEFT)){
-            cubePosition = Vector3Add(cubePosition, Vector3Negate(moveHorizontal));
+            truckPosition = Vector3Add(truckPosition, Vector3Negate(moveHorizontal));
+            truckModel.transform = MatrixRotateXYZ((Vector3){ 0.0f, DEG2RAD * 90, 0.0f });
         }
         if(IsKeyPressed(KEY_S) || IsKeyPressed(KEY_DOWN)){
-            cubePosition = Vector3Add(cubePosition, moveVertical);
+            truckPosition = Vector3Add(truckPosition, moveVertical);
+            truckModel.transform = MatrixRotateXYZ((Vector3){ 0.0f, DEG2RAD * 180, 0.0f });
         }
         if(IsKeyPressed(KEY_D) || IsKeyPressed(KEY_RIGHT)){
-            cubePosition = Vector3Add(cubePosition, moveHorizontal);
+            truckPosition = Vector3Add(truckPosition, moveHorizontal);
+            truckModel.transform = MatrixRotateXYZ((Vector3){ 0.0f, DEG2RAD * 270, 0.0f });
         }
     }
 
-    // Unloading
-    UnloadTexture(texture);
-    UnloadModel(cube);
-
-    CloseWindow();
-
+    Unload();
     return 0;
 }
---
+++ Atomdriver/src/rendering.c
@@ -0,0 +1,69 @@
+// Raylib headers
+#include "raylib.h"
+#include "raymath.h"
+#include "rlgl.h"
+#include "stdio.h"
+
+// Script headers
+#include "game.h"
+#include "rendering.h"
+
+// Window specs
+#define SCREEN_WIDTH (800)
+#define SCREEN_HEIGHT (450)
+#define WINDOW_TITLE "Atomdriver"
+
+// Camera
+Camera3D camera = { 0 };
+
+// Rendering variables
+int scaleUnit = 10;
+
+// Skybox
+Color topColor = {10, 63, 68, 255};
+Color bottomColor = {67, 153, 114};
+
+// Objects and textures
+Model truckModel;
+Texture2D truckTexture;
+
+// Initialize rendering variables
+void RenderInit(){
+    // Create a window
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE);
+    SetTargetFPS(60);
+
+    // Camera settings
+    
+    camera.position = (Vector3){ 20.0f, 20.0f, 20.0f };
+    camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };
+    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
+    camera.fovy = 45.0f;
+    camera.projection = CAMERA_ORTHOGRAPHIC;
+    // UpdateCamera(&camera, CAMERA_FREE);
+
+    // Load cube and its texture
+    truckModel = LoadModel("../assets/Models/Truck.obj");
+    truckTexture = LoadTexture("../assets/Textures/truckTexture.png");
+    truckModel.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = truckTexture;
+}
+
+// For objects to be rendered every frame (Dynamic)
+void RenderLoop(){
+    // Standard draw loop
+    BeginDrawing();
+        ClearBackground(topColor);
+        BeginMode3D(camera);
+            DrawModel(truckModel, truckPosition, scaleUnit, WHITE);
+        EndMode3D();
+    EndDrawing();
+}
+
+// Unload all models and textures, close window
+void Unload(){
+    UnloadTexture(truckTexture);
+    UnloadModel(truckModel);
+
+    CloseWindow();
+}
+
